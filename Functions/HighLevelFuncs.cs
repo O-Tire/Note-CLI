9f124ed749b0abcf59a871dd580e787c4f9ace72
O-Tire <164386722+O-Tire@users.noreply.github.com>
2025-11-19 22:41:07 +0330

    Organized the codebase. Still on v1.1.1

---
+++ Functions/HighLevelFuncs.cs
@@ -0,0 +1,107 @@
+using static LowLevelFuncs.Functions;
+
+namespace HighLevelFuncs;
+public static class Functions
+{
+    const string VERSION_NOTICE = "Note CLI - v1.1.1";
+
+    public static void NewNote(string note, string? category)
+    {
+        var notes = LoadNotes();
+
+        if (category is not null) note += "%" + category;
+        notes.Add(note);
+
+        SaveNotes(notes);
+        Console.WriteLine("Successfully added a new note.");
+    }
+
+    public static void PrintNotes()
+    {
+        var rawNotes = LoadNotes();
+        List<string> notes = new();
+        List<string> categories = new();
+        HashSet<string> uniqueCategories = new();
+
+        // Parsing the raw data into notes and categories.
+        foreach (string rawNote in rawNotes)
+        {
+            string[] notesAndCategories = rawNote.Split('%');
+            notes.Add(notesAndCategories[0]);
+            if (notesAndCategories.Length == 2)
+            {
+                categories          .Add(notesAndCategories[1]);
+                uniqueCategories    .Add(notesAndCategories[1]);
+            }
+            else
+            {
+                categories          .Add("Uncatagorized");
+                uniqueCategories    .Add("Uncatagorized");
+            }
+        }
+
+        // Printing.
+
+        Console.WriteLine("\n");
+
+        var uniqueCategoriesArray = uniqueCategories.ToArray();
+
+        for (int i = 0; i < uniqueCategoriesArray.Length; i++)
+        {
+            Console.WriteLine($"    {uniqueCategoriesArray[i]}:");
+
+            for (int j = 0; j < notes.Count; j++)
+            {
+                if (categories[j] == uniqueCategoriesArray[i])
+                {
+                    Console.WriteLine($"{j + 1}. {notes[j]}");
+                }
+            }
+
+            Console.WriteLine("\n");
+        }
+
+        Console.WriteLine("\n");
+    }
+
+    public static void RemoveNote(string stringIndex)
+    {
+        var notes = LoadNotes();
+
+        try
+        {
+            int index = Convert.ToInt16(stringIndex) - 1;
+
+            if (index + 1 > notes.Count) throw new IndexOutOfRangeException();
+
+            notes.RemoveAt(index);
+
+            SaveNotes(notes);
+            Console.WriteLine($"Successfully removed note #{index + 1}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"ERROR: Note could not be found:\n{ex.Message}");
+        }
+    }
+
+    public static void PrintVersionNotice()
+    {
+        Console.WriteLine(VERSION_NOTICE);
+    }
+
+    public static void ClearAllNotes()
+    {
+        Console.WriteLine("\nAre you sure that you want to clear all notes? (Y/N)");
+
+        if (Console.ReadKey().Key != ConsoleKey.Y)
+        {
+            Console.WriteLine("Operation canceled.");
+            return;
+        }
+
+        SaveNotes(new List<string>());
+        Console.WriteLine("All notes have been successfully cleared.");
+
+    }
+}
---
+++ Functions/LowLevelFuncs.cs
@@ -0,0 +1,37 @@
+namespace LowLevelFuncs;
+public static class Functions
+{
+    public static string GetNoteFilePath()
+    {
+        string path = Path.GetDirectoryName(Environment.ProcessPath) + "/Notes.save";
+
+        if (!File.Exists(path))
+        {
+            File.Create(path).Close();
+        }
+
+        return path;
+    }
+
+    /// <returns>Raw note and category data. Raw data are strings that contain: note + % + category(optional)</returns>
+    public static List<string> LoadNotes()
+    {
+        using (StreamReader reader = new(GetNoteFilePath()))
+        {
+            return reader.ReadToEnd().Trim().Split('\n').ToList();
+        }
+    }
+
+    /// <summary>
+    /// Saves raw note data.
+    /// </summary>
+    public static void SaveNotes(List<string> rawNoteData)
+    {
+        using (StreamWriter writer = new(GetNoteFilePath(), false))
+        {
+            writer.Write(
+            string.Join('\n', rawNoteData)
+            );
+        }
+    }
+}
